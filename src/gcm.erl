b2f167b85fbe7485213088d2b9f0f3c22f6f8a62
Klaus Trainer <klaus_trainer@posteo.de>
2013-07-04 02:27:15 +0200

    Make sure application startup works properly

--- src/gcm.erl
+++ src/gcm.erl
@@ -5,10 +5,13 @@
 
 start() ->
     ok = application:start(crypto),
+    ok = application:start(asn1),
+    ok = application:start(public_key),
+    ok = application:start(ssl),
+    ok = application:start(ibrowse),
     ok = application:start(ranch),
     ok = application:start(cowboy),
     ok = application:start(bitcask),
-    ok = application:start(ibrowse),
     ok = application:start(sasl),
     ok = application:start(lager),
     ok = application:start(gcm).
