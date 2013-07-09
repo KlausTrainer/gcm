e11f447170ced18204f2171accf69f5ff851aa91
Klaus Trainer <klaus_trainer@posteo.de>
2013-07-09 12:40:29 +0200

    Rename 'reloader' to 'gcm_reloader'

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 
-exec erl -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s gcm -s reloader
+exec erl -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s gcm -s gcm_reloader
